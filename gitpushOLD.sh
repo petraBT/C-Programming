ee2d4b829741080471f397aab8f9ce0a70b9004b
Petra Bonfert-Taylor <petra.bonfert-taylor@dartmouth.edu>
2023-08-16 16:10:34 -0400

    removed html target

---
+++ gitpushOLD.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+#
+
+echo "outdated - this was the old way of hosting..."
+echo "New way: simply use"
+echo "pretext deploy -u"
+
+# # change into html directory
+# cd output/html/
+
+# # commit and push to git
+# git add .
+# git commit -m "updating"
+# git push
